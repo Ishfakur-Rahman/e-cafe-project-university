f79ab92334d25e2785806e56f502d19760e3656d
glitchlover <samama.shahrin@gmail.com>
2022-05-31 20:14:51 +0600

    change

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -539,26 +539,6 @@ class HomePageController extends GetxController {
   }
 }
 
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home:  OfferPage(),
-    );
-  }
-}
-
-
 class OfferPage extends StatelessWidget {
   const OfferPage({Key? key}) : super(key: key);
 
