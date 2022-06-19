deed1f4a7bd17e81b26b168d65b5f7d5da7bdb73
Ishfak <mdishfakur12@gmail.com>
2022-06-19 18:25:20 +0600

    now profile data can be added and update will be available soon

--- lib/backend_api/addProfileInfo.dart
+++ lib/backend_api/addProfileInfo.dart
@@ -1,15 +1,12 @@
-import 'dart:convert';
 import 'dart:io';
 import 'package:http/http.dart' as http;
-import '../api_data_model/get_coffee_model.dart';
-import '../api_data_model/get_single_coffee_model.dart';
 import '../database/userBoxController.dart';
 
 class ProfileData {
   final token = UserBoxController().token;
   final role = UserBoxController().role;
   final shopName = UserBoxController().shopId;
-  Future<String> update_profile_data({
+  Future<String> add_profile_data({
     String? userName,
     String? contact,
     String? address,
