6267f689aa0275f4026d39c05f12631bd46d9363
Ishfak <mdishfakur12@gmail.com>
2022-06-20 20:28:10 +0600

    Profile data can be added & updated along with image

--- lib/backend_api/addProfileInfo.dart
+++ lib/backend_api/addProfileInfo.dart
@@ -12,29 +12,110 @@ class ProfileData {
     String? address,
     File? image,
   }) async {
-    http.Response response = await http.post(
-        Uri.parse('https://coffee-app-systems.herokuapp.com/profile-info/'),
-        headers: {
-          "Authorization": "Token $token"
-        },
-        body: role == 'buyer'?{
-          {
-            "user": userName,
-            "profile": image,
-            "contact": contact,
-            "address": address,
-            "shopName": shopName
-          }
-        }:{
-          "user": userName,
-          "profile": image,
-          "contact": contact,
-          "address": address,
-          "shopName": 0
-        });
-    if(response.statusCode == 200){
+    var stream = http.ByteStream(image!.openRead());
+    stream.cast();
+
+    var length = await image.length();
+
+    var uri =
+        Uri.parse('https://coffee-app-systems.herokuapp.com/profile-info/');
+
+    var request = http.MultipartRequest('POST', uri);
+
+    request.headers['Authorization'] = "Token $token";
+
+    request.fields['user'] = userName!;
+    request.fields['contact'] = contact ?? "00000000000";
+    request.fields['address'] = address ?? "add your address";
+    request.fields['shopName'] =
+        role == 'seller' ? shopName.toString() : (role == 'buyer' ? "0" : "");
+
+    var multiport = http.MultipartFile('image', stream, length);
+
+    request.files.add(multiport);
+
+    var response = await request.send();
+    // http.Response response = await http.post(
+    //     Uri.parse('https://coffee-app-systems.herokuapp.com/profile-info/'),
+    //     headers: {
+    //       "Authorization": "Token $token"
+    //     },
+    //     body: role == 'buyer'?{
+    //       {
+    //         "user": userName,
+    //         "profile": image,
+    //         "contact": contact,
+    //         "address": address,
+    //         "shopName": shopName
+    //       }
+    //     }:{
+    //       "user": userName,
+    //       "profile": image,
+    //       "contact": contact,
+    //       "address": address,
+    //       "shopName": 0
+    //     });
+    if (response.statusCode == 200) {
+      return "Added";
+    } else {
+      return "Failed to add data";
+    }
+  }
+
+  Future<String> update_profile_data({
+    required String userName,
+    String? contact,
+    String? address,
+    File? image,
+  }) async {
+
+    var stream;
+    var length;
+
+    if (image != null) {
+      stream = http.ByteStream(image.openRead());
+      stream.cast();
+      length = await image.length();
+    }
+
+    var uri = Uri.parse(
+        'https://coffee-app-systems.herokuapp.com/update-profile-info/$userName/');
+
+    var request = http.MultipartRequest('PATCH', uri);
+
+    request.headers['Authorization'] = "Token $token";
+
+    contact!=null? (request.fields['contact'] = contact): "";
+    address!=null? (request.fields['address'] = address): "";
+
+    if (image != null) {
+      var multiport = http.MultipartFile('image', stream, length);
+      request.files.add(multiport);
+    }
+    var response = await request.send();
+    // http.Response response = await http.post(
+    //     Uri.parse('https://coffee-app-systems.herokuapp.com/profile-info/'),
+    //     headers: {
+    //       "Authorization": "Token $token"
+    //     },
+    //     body: role == 'buyer'?{
+    //       {
+    //         "user": userName,
+    //         "profile": image,
+    //         "contact": contact,
+    //         "address": address,
+    //         "shopName": shopName
+    //       }
+    //     }:{
+    //       "user": userName,
+    //       "profile": image,
+    //       "contact": contact,
+    //       "address": address,
+    //       "shopName": 0
+    //     });
+    if (response.statusCode == 200) {
       return "Updated";
-    }else{
+    } else {
       return "Failed to update data";
     }
   }
