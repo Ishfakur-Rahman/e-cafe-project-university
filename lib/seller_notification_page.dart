c89e335edc805c42613eb889c8639674af931ea7
Ishfak <mdishfakur12@gmail.com>
2022-08-03 14:34:09 +0600

    added new method

---
+++ lib/seller_notification_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class NotificationSellerPage extends StatelessWidget {
+  const NotificationSellerPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
+
