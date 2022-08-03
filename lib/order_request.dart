f20945eb049844aec2096cf321eeac1cadebea22
Ishfak <mdishfakur12@gmail.com>
2022-08-03 20:46:39 +0600

    Fixed the profile creating issue

--- lib/order_request.dart
+++ lib/order_request.dart
@@ -1,12 +1,25 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:versity_project_coffee/api_data_model/get_order_model.dart';
 import 'package:versity_project_coffee/backend_api/order_data.dart';
+import 'package:versity_project_coffee/database/userBoxController.dart';
+
 class OrderRequest extends StatelessWidget {
+  List<AllOrders>? orderdata;
   void get_order() async{
-    var orders = await Order().get_all_order();
+    var order = await Order().get_all_order();
+    var orders = GetAllOrderDataModel.fromJson(jsonDecode(order)).allOrders;
+
+    for(var singleorder in orders!){
+      if(singleorder.shopName == UserBoxController().shopId){
+        orderdata?.add(singleorder);
+      }
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    get_order();
     return Scaffold(
       backgroundColor: const Color(0xFF212121),
       appBar: AppBar(title: const Text('Orders Requested'),),
